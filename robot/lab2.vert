5b26953407e20028294e11b3498bb90e6de48cd4
karivmcmahon <kmc_xo@hotmail.co.uk>
2014-10-25 15:12:26 +0100

    robot with hat

--- robot/lab2.vert
+++ robot/lab2.vert
@@ -1,15 +1,37 @@
 // Vertex shader with a model transformation uniform
 
 #version 400
-layout(location = 0) in vec4 position;
+layout(location = 0) in vec3 position;
 layout(location = 1) in vec4 colour;
+layout(location = 2) in vec3 normal;
 out vec4 fcolour;
 uniform mat4 model, view;
 
 void main()
 {
-	gl_Position = view * model * position;
+
+	vec3 lightDirection = vec3(2,1.0,1.0);
+	mat4 modelViewMatrix = view * model;
+	vec4 position4 = vec4(position, 1.0);
+    mat3 normalMatrix = mat3(modelViewMatrix);
+	vec3 transNormal = vec3(normalMatrix * normal);
+	lightDirection = normalize(lightDirection);
+	transNormal = normalize(transNormal);
+	float diffuseComponent = max(dot(transNormal,lightDirection),0.0);
+	vec4 diffuse_colour = vec4(0.0, 0.0, 1.0, 1.0);
 	
-	fcolour = colour;
-	//fcolour = position * 2.0 + vec4(0.5, 0.5, 0.5, 1.0);
+	vec4 diffuseLighting = vec4( diffuse_colour * diffuseComponent);
+	vec4 ambient = diffuse_colour * 0.3;
+	
+	vec4 transPosition = position4 * modelViewMatrix;
+	float shininess = 8.0;
+	vec3 v = normalize(-transPosition.xyz);
+	vec3 r = reflect(-lightDirection,transNormal);
+	vec3 specular = pow(max(dot(r,v),0.0), shininess) * vec3(1.0,1.0,1.0);
+	
+	fcolour = ambient + diffuseLighting + specular;
+	
+	gl_Position =   view * model  * position4;
+	
+	
 }
--- robot/lab2start.cpp
+++ robot/lab2start.cpp
@@ -24,17 +24,22 @@ if you prefer */
 #include "glm/gtc/matrix_transform.hpp"
 #include <glm/gtc/type_ptr.hpp>
 #include <stack>
+#include <vector>
+#include <cmath>
 
-GLuint positionBufferObject, colourObject;
+GLuint positionBufferObject, normalsBufferObject, colourObject, coneBufferObj, coneColourObj, coneNormalObj;
 GLuint program;
 GLuint vao;
 
 /* Position and view globals */
-GLfloat angle_x, armangle, angle_x_inc, anglex;
-
+GLfloat angle_x, armangle, angle_x_inc, anglex, coneangle, xr, x ,y;
 /* Uniforms*/
 GLuint modelID, viewID;
+GLfloat pi = 3.1415926535898;
 std::stack<glm::mat4> model;
+std::vector<GLfloat> conePositions;
+std::vector<GLfloat> coneColours;
+std::vector<GLfloat> coneNormals;
 
 /*
 This function is called before entering the main rendering loop.
@@ -42,119 +47,69 @@ Use it for all your initialisation stuff
 */
 void init(GLWrapper *glw)
 {
-	angle_x = -90;
+	coneangle = -90;
+	angle_x = 90;
 	anglex = 0;
 	angle_x_inc = 0;
 	armangle = 180;
-
+	xr = 1.0f;
+	x = 0;
+	y = 0;
 	// Generate index (name) for one vertex array object
 	glGenVertexArrays(1, &vao);
 
 	// Create the vertex array object and make it current
 	glBindVertexArray(vao);
 
-	/* Define vertices for a cube in 12 triangles */
 	GLfloat vertexPositions[] =
 	{
-		//Cube 1
-		-0.25f, 0.25f, -0.25f, 1.f,
-		-0.25f, -0.25f, -0.25f, 1.f,
-		0.25f, -0.25f, -0.25f, 1.f,
+		-0.25f, 0.25f, -0.25f,
+		-0.25f, -0.25f, -0.25f,
+		0.25f, -0.25f, -0.25f,
 
-		0.25f, -0.25f, -0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, -0.25f, 1.f,
+		0.25f, -0.25f, -0.25f,
+		0.25f, 0.25f, -0.25f,
+		-0.25f, 0.25f, -0.25f,
 
-		0.25f, -0.25f, -0.25f, 1.f,
-		0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
+		0.25f, -0.25f, -0.25f,
+		0.25f, -0.25f, 0.25f,
+		0.25f, 0.25f, -0.25f,
 
-		0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
+		0.25f, -0.25f, 0.25f,
+		0.25f, 0.25f, 0.25f,
+		0.25f, 0.25f, -0.25f,
 
-		0.25f, -0.25f, 0.25f, 1.f,
-		-0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
+		0.25f, -0.25f, 0.25f,
+		-0.25f, -0.25f, 0.25f,
+		0.25f, 0.25f, 0.25f,
 
-		-0.25f, -0.25f, 0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
+		-0.25f, -0.25f, 0.25f,
+		-0.25f, 0.25f, 0.25f,
+		0.25f, 0.25f, 0.25f,
 
-		-0.25f, -0.25f, 0.25f, 1.f,
-		-0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
+		-0.25f, -0.25f, 0.25f,
+		-0.25f, -0.25f, -0.25f,
+		-0.25f, 0.25f, 0.25f,
 
-		-0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
+		-0.25f, -0.25f, -0.25f,
+		-0.25f, 0.25f, -0.25f,
+		-0.25f, 0.25f, 0.25f,
 
-		-0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, -0.25f, -0.25f, 1.f,
+		-0.25f, -0.25f, 0.25f,
+		0.25f, -0.25f, 0.25f,
+		0.25f, -0.25f, -0.25f,
 
-		0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, -0.25f, 0.25f, 1.f,
+		0.25f, -0.25f, -0.25f,
+		-0.25f, -0.25f, -0.25f,
+		-0.25f, -0.25f, 0.25f,
 
-		-0.25f, 0.25f, -0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
+		-0.25f, 0.25f, -0.25f,
+		0.25f, 0.25f, -0.25f,
+		0.25f, 0.25f, 0.25f,
 
-		0.25f, 0.25f, 0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
-		-0.25f, 0.25f, -0.25f, 1.f,
-
-		//Cube 1
-		- 0.25f, 0.25f, -0.25f, 1.f,
-		-0.25f, -0.25f, -0.25f, 1.f,
-		0.25f, -0.25f, -0.25f, 1.f,
-
-		0.25f, -0.25f, -0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, -0.25f, 1.f,
-
-		0.25f, -0.25f, -0.25f, 1.f,
-		0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
-
-		0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
-
-		0.25f, -0.25f, 0.25f, 1.f,
-		-0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
-
-		-0.25f, -0.25f, 0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
-
-		-0.25f, -0.25f, 0.25f, 1.f,
-		-0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
-
-		-0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, -0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
-
-		-0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, -0.25f, 0.25f, 1.f,
-		0.25f, -0.25f, -0.25f, 1.f,
-
-		0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, -0.25f, -0.25f, 1.f,
-		-0.25f, -0.25f, 0.25f, 1.f,
-
-		-0.25f, 0.25f, -0.25f, 1.f,
-		0.25f, 0.25f, -0.25f, 1.f,
-		0.25f, 0.25f, 0.25f, 1.f,
-
-		0.25f, 0.25f, 0.25f, 1.f,
-		-0.25f, 0.25f, 0.25f, 1.f,
-		-0.25f, 0.25f, -0.25f, 1.f
-
-		
+		0.25f, 0.25f, 0.25f,
+		-0.25f, 0.25f, 0.25f,
+		-0.25f, 0.25f, -0.25f,
 	};
 
 	/* Define an array of colours */
@@ -204,6 +159,22 @@ void init(GLWrapper *glw)
 
 	};
 
+	GLfloat normals[] =
+	{
+		0, 0, -1.f, 0, 0, -1.f, 0, 0, -1.f,
+		0, 0, -1.f, 0, 0, -1.f, 0, 0, -1.f,
+		1.f, 0, 0, 1.f, 0, 0, 1.f, 0, 0,
+		1.f, 0, 0, 1.f, 0, 0, 1.f, 0, 0,
+		0, 0, 1.f, 0, 0, 1.f, 0, 0, 1.f,
+		0, 0, 1.f, 0, 0, 1.f, 0, 0, 1.f,
+		-1.f, 0, 0, -1.f, 0, 0, -1.f, 0, 0,
+		-1.f, 0, 0, -1.f, 0, 0, -1.f, 0, 0,
+		0, -1.f, 0, 0, -1.f, 0, 0, -1.f, 0,
+		0, -1.f, 0, 0, -1.f, 0, 0, -1.f, 0,
+		0, 1.f, 0, 0, 1.f, 0, 0, 1.f, 0,
+		0, 1.f, 0, 0, 1.f, 0, 0, 1.f, 0,
+	};
+
 	/* Create a vertex buffer object to store vertices */
 	glGenBuffers(1, &positionBufferObject);
 	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
@@ -214,6 +185,102 @@ void init(GLWrapper *glw)
 	glGenBuffers(1, &colourObject);
 	glBindBuffer(GL_ARRAY_BUFFER, colourObject);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColours), vertexColours, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glGenBuffers(1, &normalsBufferObject);
+	glBindBuffer(GL_ARRAY_BUFFER, normalsBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, 36 * sizeof(glm::vec3), normals, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+
+	conePositions.push_back(0.0f);
+	conePositions.push_back(0.0f);
+	conePositions.push_back(0.75f);
+	
+	
+
+	coneColours.push_back(0.0f);
+	coneColours.push_back(0.0f);
+	coneColours.push_back(1.0f);
+	coneColours.push_back(1.0f);
+
+	GLfloat twicePi = 2.0f * pi;
+	for (GLfloat angle = 0.0; angle <= 90; angle++)
+	{
+
+
+		// SEPERATE LOOP FOR NORMAL AFTER ALL POINTS RETRIEVED ??
+		glm::vec3 normal = (cos(0.75f) * glm::vec3(cos(angle), 0, sin(angle)) + sin(angle) * glm::vec3(0, 1, 0));
+		coneNormals.push_back(normal[0]);
+		coneNormals.push_back(normal[1]);
+		coneNormals.push_back(normal[2]);
+
+
+		conePositions.push_back(x + (cos(angle * twicePi / 20) * 5.0f));
+		conePositions.push_back(y + (sin(angle * twicePi / 20) * 5.0f));
+		conePositions.push_back(0.0f);
+		
+
+		coneColours.push_back(0.0f);
+		coneColours.push_back(0.0f);
+		coneColours.push_back(1.0f);
+		coneColours.push_back(1.0f);
+
+
+
+	}
+
+	std::cout << conePositions.size() << "\n";
+
+	conePositions.push_back(0.0f);
+	conePositions.push_back(0.0f);
+	conePositions.push_back(0.0f);
+	
+
+	for (GLfloat angle = 0.0; angle <= 90; angle++)
+	{
+
+
+		glm::vec3 normal = (cos(0.0f) * glm::vec3(cos(angle), 0, sin(angle)) + sin(angle) * glm::vec3(0, 1, 0));
+		coneNormals.push_back(normal[0]);
+		coneNormals.push_back(normal[1]);
+	     coneNormals.push_back(normal[2]);
+
+
+		 conePositions.push_back(x + (cos(angle * twicePi / 20) * 5.0f));
+		 conePositions.push_back(y + (sin(angle * twicePi / 20) * 5.0f));
+		conePositions.push_back(0.0f);
+		
+
+		coneColours.push_back(0.0f);
+		coneColours.push_back(0.0f);
+		coneColours.push_back(1.0f);
+		coneColours.push_back(1.0f);
+
+
+
+	}
+
+
+	std::cout << conePositions.size() << "\n";
+
+	/* Create a vertex buffer object to store vertices */
+	glGenBuffers(1, &coneBufferObj);
+	glBindBuffer(GL_ARRAY_BUFFER, coneBufferObj);
+	glBufferData(GL_ARRAY_BUFFER, conePositions.size(), &conePositions[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	/* Create a vertex buffer object to store vertex colours */
+	glGenBuffers(1, &coneColourObj);
+	glBindBuffer(GL_ARRAY_BUFFER, coneColourObj);
+	glBufferData(GL_ARRAY_BUFFER, coneColours.size(), &coneColours[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	/* Create a vertex buffer object to store vertex colours */
+	glGenBuffers(1, &coneNormalObj);
+	glBindBuffer(GL_ARRAY_BUFFER, coneNormalObj);
+	glBufferData(GL_ARRAY_BUFFER, coneNormals.size(), &coneNormals[0], GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	try
@@ -251,7 +318,7 @@ void display()
 	/* glVertexAttribPointer(index, size, type, normalised, stride, pointer) 
 	   index relates to the layout qualifier in the vertex shader and in 
 	   glEnableVertexAttribArray() and glDisableVertexAttribArray() */
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, colourObject);
 	glEnableVertexAttribArray(1);
@@ -261,15 +328,10 @@ void display()
 	glEnableVertexAttribArray() and glDisableVertexAttribArray() */
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
-	// Model matrix : an identity matrix (model will be at the origin)
-	//model.push(glm::mat4(1.0f));
-	//model.top() = glm::translate(model.top(), glm::vec3(x2, y2, 0));
-	//model.top() = glm::scale(model.top(), glm::vec3(scale, scale, scale));
-	//model.top() = glm::rotate(model.top(), -angle_x, glm::vec3(1, 0, 0)); //rotating in clockwise direction around x-axis
-	//model.top() = glm::rotate(model.top(), -angle_z, glm::vec3(0, 0, 1)); //rotating in clockwise direction around x-axis
-	//glUniformMatrix4fv(modelID, 1, GL_FALSE, &(model.top())[0][0]);
-	//model.pop();
-	//glDrawArrays(GL_TRIANGLES, 0, 36);
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, normalsBufferObject);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
 		glm::vec3(0, 0, 0.1), // Camera is at (0,0,4), in World Space
@@ -277,33 +339,8 @@ void display()
 		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 		);
 
-	//if (anglex >= 30.0f && anglex <= 90.0f)
-	//{
-	//	std::cout << "hi";
-	//	View = glm::lookAt(
-	//		glm::vec3(0, 0, 1), // Camera is at (0,0,4), in World Space
-	//		glm::vec3(0, 0, 0), // and looks at the origin
-	//	glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-	//		);
-	//}
-	//else if (anglex <= -1.0f)
-	//{
-	//	std::cout << "hi";
-	//	View = glm::lookAt(
-	//		glm::vec3(0.2, 0, 1), // Camera is at (0,0,4), in World Space
-	//		glm::vec3(0, 0, 0), // and looks at the origin
-	//		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-	//		);
-	//}
-
-	model.push(glm::mat4(1.0f));
-	
-	model.top() = glm::rotate(model.top(), -anglex, glm::vec3(0, 1, 0));
 
 
-	glUniformMatrix4fv(modelID, 1, GL_FALSE, &(model.top())[0][0]);
-	glUniformMatrix4fv(viewID, 1, GL_FALSE, &View[0][0]);
-	model.pop();
 	 //Model matrix : an identity matrix (model will be at the origin)
 	model.push(glm::mat4(1.0f));
 	model.top() = glm::rotate(model.top(), -anglex, glm::vec3(0, 1, 0));
@@ -312,7 +349,7 @@ void display()
 	glUniformMatrix4fv(modelID, 1, GL_FALSE, &(model.top())[0][0]);
 	glUniformMatrix4fv(viewID, 1, GL_FALSE, &View[0][0]);
 	model.pop();
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	//glDrawArrays(GL_TRIANGLES, 0, 36);
 
 	// Model matrix : an identity matrix (model will be at the origin)
 	model.push(glm::mat4(1.0f));
@@ -376,9 +413,85 @@ void display()
 	model.pop();
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 
+	model.push(glm::mat4(1.0f));
+	model.top() = glm::rotate(model.top(), -anglex, glm::vec3(0, 1, 0));
+	model.top() = glm::scale(model.top(), glm::vec3(-0.20, 0.2, -0.4));
+	model.top() = glm::translate(model.top(), glm::vec3(-0.50, -4.25, 0.06)); //rotating in clockwise direction around x-axis
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &(model.top())[0][0]);
+	glUniformMatrix4fv(viewID, 1, GL_FALSE, &View[0][0]);
+	model.pop();
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	model.push(glm::mat4(1.0f));
+	model.top() = glm::rotate(model.top(), -anglex, glm::vec3(0, 1, 0));
+	model.top() = glm::scale(model.top(), glm::vec3(-0.20, 0.2, -0.4));
+	model.top() = glm::translate(model.top(), glm::vec3(0.50, -4.25, 0.06)); //rotating in clockwise direction around x-axis
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &(model.top())[0][0]);
+	glUniformMatrix4fv(viewID, 1, GL_FALSE, &View[0][0]);
+	model.pop();
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
 
 	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, coneBufferObj);
+	glEnableVertexAttribArray(0);
+
+	/* glVertexAttribPointer(index, size, type, normalised, stride, pointer)
+	index relates to the layout qualifier in the vertex shader and in
+	glEnableVertexAttribArray() and glDisableVertexAttribArray() */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, coneColourObj);
+	glEnableVertexAttribArray(1);
+
+	/* glVertexAttribPointer(index, size, type, normalised, stride, pointer)
+	index relates to the layout qualifier in the vertex shader and in
+	glEnableVertexAttribArray() and glDisableVertexAttribArray() */
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glFrontFace(GL_CW);
+
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, coneNormalObj);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	model.push(glm::mat4(1.0f));
+	//model.top() = glm::rotate(model.top(), xr, glm::vec3(1, 0, 0));
+	//model.top() = glm::rotate(model.top(), xr, glm::vec3(0, 1, 0));
+    model.top() = glm::translate(model.top(), glm::vec3(0, 0.6, 0)); //rotating in clockwise direction around x-axis
+	
+	model.top() = glm::scale(model.top(), glm::vec3(0.02, 0.2, 0.3));
+	model.top() = glm::rotate(model.top(), -anglex, glm::vec3(0, 1, 0));
+	
+	model.top() = glm::rotate(model.top(), -angle_x, glm::vec3(1, 0, 0));
+
+	
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &(model.top())[0][0]);
+	glUniformMatrix4fv(viewID, 1, GL_FALSE, &View[0][0]);
+	model.pop();
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 1086);
+
+	glFrontFace(GL_CW);
+
+	model.push(glm::mat4(1.0f));
+	//model.top() = glm::rotate(model.top(), xr, glm::vec3(1, 0, 0));
+	//model.top() = glm::rotate(model.top(), xr, glm::vec3(0, 1, 0));
+	model.top() = glm::translate(model.top(), glm::vec3(0, 0.6, 0)); //rotating in clockwise direction around x-axis
+	model.top() = glm::scale(model.top(), glm::vec3(0.02, 0.2, 0.3));
+	model.top() = glm::rotate(model.top(), -anglex, glm::vec3(0, 1, 0));
+	model.top() = glm::rotate(model.top(), -angle_x, glm::vec3(1, 0, 0));
+	
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &(model.top())[0][0]);
+	glUniformMatrix4fv(viewID, 1, GL_FALSE, &View[0][0]);
+	model.pop();
+   glDrawArrays(GL_TRIANGLE_FAN, 1086, 2172);
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
 	glUseProgram(0);
+
+	
 	
 
 }
@@ -401,12 +514,14 @@ static void keyCallback(GLFWwindow* window, int k, int s, int action, int mods)
 	if (k == 'Q')
 	{
 		anglex += 10.0f;
-		armangle += 1.0f;
+		armangle += 0.02f;
+		//angle_x += 10.0f;
 	}
 	if (k == 'W')
 	{
 		anglex -= 10.0f; 
-		armangle -= 1.0f;
+		armangle -= 0.02f;
+		//angle_x -= 10.0f;
 	}
 
 }
